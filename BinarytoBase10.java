3db39908455d0a084beade19d864202e3bab98fe
Kshitij Shah <kshnik@gmail.com>
2018-04-10 22:25:15 -0400

    Update BinarytoBase10.java

--- BinarytoBase10.java
+++ BinarytoBase10.java
@@ -17,16 +17,16 @@ public class BinarytoBase10{
 	    return 0;
 	} else {
 	int b;
-	return b = 2 * base (a/10) + a%10; 
+	return b = 2 * base(a / 10) + a % 10; 
 	}
     }
 
-    public static void main (String str[])throws IOException {
+    public static void main (String str[]) throws IOException {
 	BufferedReader stdin = new BufferedReader (new InputStreamReader (System.in));
 	System.out.println("Binary to Base 10 Converter: \n\n");
-	System.out.print ("What binary do you want to change the base of?:  ");
-	int a = Integer.parseInt (stdin.readLine ());
-	System.out.println ("The number in base 10 is " + base (a));
+	System.out.print("What binary do you want to change the base of?:  ");
+	int a = Integer.parseInt(stdin.readLine ());
+	System.out.println("The number in base 10 is " + base(a));
     }
 }
 
